d54c4b955860461cf16669841c3fa9eefc8bac8d
rodrigo <rodrigog3wconcept@gmail.com>
2026-01-22 17:09:59 -0300

    Test: Add more integration tests to user

---
+++ tests/integration_tests/test_account.py
@@ -0,0 +1,2 @@
+#pylint: disable=redefined-outer-name
+#pylint: disable=unused-argument
--- tests/integration_tests/test_user.py
+++ tests/integration_tests/test_user.py
@@ -40,3 +40,41 @@ def test_delete_user(db_session, fake_user):
     db_session.commit()
     delete_user = db_session.query(UserEntity).filter_by(id=fake_user.id).first()
     assert delete_user is None
+
+def test_find_user_by_id(db_session, fake_user):
+    found_user = db_session.query(UserEntity).filter_by(id=fake_user.id).first()
+    assert found_user is not None
+
+
+def test_find_all_users(db_session, fake_user):
+    users = db_session.query(UserEntity).all()
+    assert len(users) >= 1
+
+def test_uniq_email(db_session, fake_user):
+    # In this test, I used fake_user to don't need to create another user
+    """I insert a user with an email that already exists to test the unique constraint.
+       try to commit and wait that db raise an exception (IntegrityError).
+       pytest.raises(Exception) is used to check if the exception is raised.
+       after that, I rollback the session to clean the state.
+    """
+    another_user = UserEntity(
+        first_name="Carlos",
+        last_name="Pereira",
+        age=35,
+        email="ana.silva@example.com",  # Duplicate email
+        phone="192837465",
+        is_active=True,
+        password_hash="hashed_password_3",
+        created_at=datetime.now(),
+        updated_at=None
+    )
+    db_session.add(another_user)
+    with pytest.raises(Exception):
+        db_session.commit()
+    db_session.rollback()
+
+def test_inactive_user(db_session, fake_user):
+    fake_user.is_active = False
+    db_session.commit()
+    inactive_user = db_session.query(UserEntity).filter_by(id=fake_user.id).first()
+    assert inactive_user.is_active is False
